246e2509dee15a7cf04e47486ebf2dab36cc14d9
eccentric <zhosan00@list.ru>
2024-03-20 21:29:53 +0300

    adding project

---
+++ MedicineFWD/src/main/java/com/example/MedicineFWD/MedicineFWD/MedicineFwdApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedicineFWD.MedicineFWD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineFwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineFwdApplication.class, args);
+	}
+
+}
---
+++ MedicineFWD/src/test/java/com/example/MedicineFWD/MedicineFWD/MedicineFwdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedicineFWD.MedicineFWD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineFwdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
